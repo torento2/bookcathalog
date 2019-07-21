aab4b41f73be65714d7b941ecb9cd8fb6d22d03b
Konrad Kaczmarski <obeese26@gmail.com>
2019-07-21 10:46:20 +0200

    sixth commit

--- src/main/java/com/github/torento2/bookcatalog/controller/BookController.java
+++ src/main/java/com/github/torento2/bookcatalog/controller/BookController.java
@@ -1,10 +1,13 @@
 package com.github.torento2.bookcatalog.controller;
 import com.github.torento2.bookcatalog.service.BookService;
+import model.Book;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.websocket.server.PathParam;
 
@@ -16,7 +19,15 @@ public class BookController {
     public BookController(BookService bookService) {
         this.bookService = bookService;
     }
+    @GetMapping("/")
+    public String index( ) {
+        return "forward:/book-list";
+    }
+    @PostMapping("/save-book")
+    public String saveBook(@ModelAttribute Book book) {
+        return "redirect:/book-list";
 
+    }
     @GetMapping("/book-list")
     public String bookList(Model model) {
         model.addAttribute("books" , bookService.getBooks());
@@ -29,7 +40,13 @@ public class BookController {
     }
     @GetMapping("/book-details")
     public String bookDetail1(Model model, @PathParam("id") Long id) {
+        model.addAttribute("book", bookService.getBook(id));
         return "book-detail";
+    }
+    @GetMapping("/add-book")
+    public String addBook(Model model, @PathParam("id") Long id) {
+        model.addAttribute("book", new Book());
+        return "add-book";
     }
 
 }
